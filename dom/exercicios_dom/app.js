8ccbbcd6b5bc892f537855b6b8b3351a09f35cf5
MatheusReis97 <Matheus.drs97@hotmail.com>
2023-03-24 21:18:23 -0300

    realização de 10 exercicios;

--- dom/exercicios_dom/app.js
+++ dom/exercicios_dom/app.js
@@ -1,4 +1,4 @@
-new Vue ({
+new Vue({
     el: '#desafio',
     data: {
         nome: 'Matheus',
@@ -7,26 +7,45 @@ new Vue ({
         imagem: 'https://www.ahnegao.com.br/wp-content/uploads/2013/08/sailor.jpg',
         numero: 0,
         valor: '',
-        
+        valor2: 0,
+        resultado: 0,
+
     },
 
+watch: {
+    resultado(valor)
+    valor2(){
+        setTimeout(()=>{
+            this.valor2=0;
+        },5000 )   }
 
-    methods: {
-        nomeCompleto(){
-            console.log(this);
+},
+
+
+methods: {
+    nomeCompleto(){
+        console.log(this);
         return ` Meu nome é ${this.nome} ${this.sobrenome} e tenho ${this.idade} anos`
-        },
+    },
 
-        numAleatorio (){
-            return Math.random() ;
-        },
+    numAleatorio(){
+        return Math.random();
+    },
 
-        buttonAlerta (){
-            window.alert('Tamo junto')
-        },
+    buttonAlerta(){
+        window.alert('Tamo junto')
+    },
 
-        amrValor (event){
-            this.valor = event.target.value;
-        }
+    amrValor(event){
+        this.valor = event.target.value;
+    },
+    altTexto() {
+
+        return this.valor2 == 37 ? this.resultado = "Valor igual" : this.resultado = "Valor diferente";
+      
     }
-})
+
+
+},
+    }
+)
