3a33fcfb39244d1f27b59c0fbcf6f25670b93947
MatheusReis97 <Matheus.drs97@hotmail.com>
2023-03-30 21:17:59 -0300

    commit: "adc condicoinais-e-lista"

---
+++ condicionais-e-listas/js/script.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: '#app' ,
+    data() {
+
+        return{
+                nome: 'Fabricio Bobão',
+                logado: false,
+                anonimo: false,
+                cores: ['VERMELHO', 'VERDE', 'AMARELO' ,'AZUL', 'PRETÃO']
+        }
+    }
+});
